6737456d9c25bdeaa1c1cc236a022c084ff1fd76
LM <lewlimin@gmail.com>
2016-08-05 14:48:25 +0800

    updated player dadashboard + text alignment

--- tic-tac-jq.js
+++ tic-tac-jq.js
@@ -9,32 +9,37 @@ $(document).ready(function() {
    $('.grid').html(" ");
  })
 
-$('.grid').click(function () {
-   console.log("click");
-   var idNumber = parseInt(this.id);
-   var currentPlayer = player;
-   if (playTurn(idNumber)) {
-   $(this).text(currentPlayer);
- }
+  $('.grid').click(function () {
+     console.log("click");
+     var idNumber = parseInt(this.id);
+     var currentPlayer = player;
+     if (playTurn(idNumber)) {
+       $(this).text(currentPlayer);
+     }
 
-winner =whoWon()
-if (winner ===1) {
-  console.log("player 1 wins")
-  alert(("player 1 wins"))
-  };
-if (winner ===2) {
-  console.log("player 2 wins")
-  alert(("player 2 wins"))
-  };
-if (winner ===3) {
-  console.log("its a tie")
-  alert(("its a tie"))
-  };
+    winner =whoWon()
+    if (winner ===1) {
+      console.log("player 1 wins")
+      alert(("player 1 wins"))
+      };
+    if (winner ===2) {
+      console.log("player 2 wins")
+      alert(("player 2 wins"))
+      };
+    if (winner ===3) {
+      console.log("its a tie")
+      alert(("its a tie"))
+      };
 
-if (isGameOver()=== true ) {
-  console.log("there is a winner")
-  restart()
-  window.location.reload() }
+    if (isGameOver()=== true ) {
+      console.log("there is a winner")
+      restart()
+      window.location.reload()
+    }
 
-  });
+      //Update the Player data below
+    $('.whichplayer').text("Player " + currentPlayer);
+
+    });
+
   });
